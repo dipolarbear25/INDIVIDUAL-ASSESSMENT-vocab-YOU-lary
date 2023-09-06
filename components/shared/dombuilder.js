47f5a6af4413395da69ce2114188f3c45721430f
Austin Mangum <mangumaustin@gmail.com>
2023-09-06 12:35:48 -0500

    added a dom builder with a main container

---
+++ components/shared/dombuilder.js
@@ -0,0 +1,15 @@
+import renderToDOM from '../../utils/renderToDom';
+
+const domBuilder = () => {
+  const domString = `
+  <div id="main-container">
+    <div id="add-button"></div>
+    <div id="card-container"></div>
+    <div id="vocabulary-btn"></div>
+    <div id="definition-btn"></div>
+  </div>`;
+  // id like to add 2 buttons to set all cards to either a definition side or the vocabulary side
+  renderToDOM('#app', domString);
+};
+
+export default domBuilder;
